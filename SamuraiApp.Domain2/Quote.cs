984c437b4493df7701e8482e4f7748b152e64ff1
Everton Mota <mcostagt@gmail.com>
2018-10-19 13:55:05 -0300

    first commit

---
+++ SamuraiApp.Domain2/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace SamuraiApp.Domain2
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Samurai samurai{ get; set; }
+        public int SamuraiId{ get; set; }
+    }
+}
